88024a3def324b2f49cfc29e0b5aabe3ee3bc0d8
Mu- <Mu@gmail.com>
2024-07-24 19:45:00 +0900

    index.js

--- theme/index.js
+++ theme/index.js
@@ -1,10 +1,10 @@
 const customTheme = {
     titleColor: "#f3f3f3",
     textColor: "#a8a8a8",
-    bgColor: "#151515",
+    bgColor: "#8b0000",
     theme: {
       lang: {
-        JupyterNotebook: { color: "#DA5B0B" },
+        JupyterNote: { color: "#DA5B0B" },
         Python: { color: "#3572A5" },
         Ruby: { color: "#701516" }
       }
